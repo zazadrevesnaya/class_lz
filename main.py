7dab7fb4f9fe8133320d27fdab0e85ec2fb78b9c
zazadrevesnaya <petroosetro0@gmail.com>
2025-06-10 00:02:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ def main():
     print("For octagon 1:")
     octagon1 = Octagon(5)
     octagon1.display_all_info()
-
+    octagon1.draw_octagon()
 
     print("\nFor octagon 2:")
     octagon2 = Octagon(8)
     octagon2.display_all_info()
-
+    octagon2.draw_octagon()
+    
 if __name__ == '__main__':
         main()
